6d6395ed1bed483726d62ce9225321e38b6aff80
Aleksei Belousov <vs.oko@rambler.ru>
2022-12-01 17:16:08 +0300

    deploy test

---
+++ hbs.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // data: 'src/markup/data',
+    // decorators: 'src/markup/decorators',
+    // helpers: 'src/markup/helpers',
+    layouts: 'src/layouts',
+    partials: 'src/partials',
+};
